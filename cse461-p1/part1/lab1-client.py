376286dd180a1395bcd872d1d28c140ce2c39348
Medha Mittal <mmittal2@cs.washington.edu>
2026-02-02 00:52:55 -0800

    Part a and part b working locally

--- cse461-p1/part1/lab1-client.py
+++ cse461-p1/part1/lab1-client.py
@@ -2,16 +2,16 @@ import socket
 import struct
 import time
 import errno
-import numpy as np
 
 import sys
 
 CONFIG_STUID = 811
 STEP = 1
-SERVER_ADDR: str = "attu2.cs.washington.edu"
+SERVER_ADDR: str = sys.argv[1]
 #SERVER_ADDR: str = "attu3.cs.washington.edu"
-PORT = 41201
+PORT = int(sys.argv[2])
 HOST = socket.gethostbyname(SERVER_ADDR)
+BUF_SIZE = 1024
 # packet_data = struct.pack("iihh", payload_len, psecret, STEP, CONFIG_STUID)
 
 def stage_a():
@@ -21,7 +21,7 @@ def stage_a():
     print ("UDP target Port:", PORT)
     msg = b"hello world\x00"
 
-    header = struct.pack("iihh", len(msg), 0, STEP, CONFIG_STUID)
+    header = struct.pack("!iihh", len(msg), 0, STEP, CONFIG_STUID)
     packet = header + msg
 
     # send to server
@@ -29,16 +29,16 @@ def stage_a():
     print(f"Bytes sent: {sent}")
     # get reponse
     try:
-        data, server_addr = sock.recvfrom(41201)	
+        data, server_addr = sock.recvfrom(BUF_SIZE)	
         print(f"Received response from {server_addr}")
         if len(data) < 12:
             return None
-        payload_len, psecret, step, student_id = struct.unpack("iihh", data[:12])
+        payload_len, psecret, step, student_id = struct.unpack("!iihh", data[:12])
         # fix syntax
         if not(payload_len, psecret, step, student_id):
                 sock.close()
                 return None
-        num, len_val, udp_port, secretA = struct.unpack("iiii", data[12:28])
+        num, len_val, udp_port, secretA = struct.unpack("!iiii", data[12:28])
         print(f"num: {num}")
         print(f"len: {len_val}")
         print(f"udp_port: {udp_port}")
@@ -48,40 +48,50 @@ def stage_a():
         print("Timeout")
         sock.close()
         return None
+
+
 def stage_b(num, lenA, udp_port, secretA, sock):
     stage_b_socket = sock
     stage_b_socket.settimeout(5)
     # need to change addr?? 
 
-    #tcp uses packets
+    #udp uses packets
     packets = []
-    sent = [False] * num
+    # sent = [False] * num
     for i in range(num):
-        payload = struct.pack("i", i)
-        header =struct.pack("iihh", len(payload), secretA, 1, CONFIG_STUID)
-        packets.append(header+payload)
+        payload = struct.pack("!i", i)
+        payload += b'\x00' * lenA
+        header = struct.pack("!iihh", len(payload), secretA, 1, CONFIG_STUID)
+        packets.append(header + payload)
+
+    num_sent = 0
     
-    while True:
-        for i in range(num):
-            if not sent[i]:
-                stage_b_socket.sendto(packets[i], SERVER_ADDR)
+    while num_sent < num:
+        stage_b_socket.sendto(packets[num_sent], (SERVER_ADDR, udp_port))
         try:
-            data, client_addr = stage_b_socket.recvfrom(41201)	
-            payload_len, psecret, step, student_id = struct.unpack("iihh", data[:12])
-            #
-            sent_id = struct.unpack("i", data[12:16])[0]
-            if psecret == secretA and step == 2:
-                sent[sent_id] = True
+            data, client_addr = stage_b_socket.recvfrom(BUF_SIZE)
+            acked_packet_id = struct.unpack("!i", data[12:])
+            print("acked_packet_id:" + str(acked_packet_id[0]) + "num_sent" + str(num_sent))
+            if acked_packet_id[0] == num_sent:
+                num_sent += 1
         except socket.timeout:
             continue
-    data, client_addr = stage_b_socket.recvfrom(41201)	
-    tcp_port, secretB = struct.unpack("ii", data[12:20])
+
+    print("success!")
+    
+    return 0, 0
+
+    """
+    data, client_addr = stage_b_socket.recvfrom(BUF_SIZE)	
+    tcp_port, secretB = struct.unpack("!ii", data[12:20])
     print("B:", secretB)
-    return tcp_port, secretB        
+    return tcp_port, secretB 
+    """       
         
-    if __name__ == "__main__":
-        num, lenA, udp_port, secretA, sock = stage_a()
-        tcp_port, secretB = stage_b(num, lenB, udp_port, secretA, sock)
+
+if __name__ == "__main__":
+    num, lenA, udp_port, secretA, sock = stage_a()
+    tcp_port, secretB = stage_b(num, lenA, udp_port, secretA, sock)
 
 
 # tcp 
--- cse461-p1/part1/run_client.sh
+++ cse461-p1/part1/run_client.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 dname=$(dirname ${BASH_SOURCE[0]})
-python3 $dname/client.py $1 $2
+python3 $dname/lab1-client.py $1 $2
